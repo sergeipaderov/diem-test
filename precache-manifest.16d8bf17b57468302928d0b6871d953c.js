3620b9463e6c0f3db684c26aeeac6994002c4285
SergPaderov <sergeipaderov@gmail.com>
2019-08-20 16:42:57 -0300

    Updates

---
+++ precache-manifest.16d8bf17b57468302928d0b6871d953c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633abf00e4c488b3eb053db0834e8d43",
+    "url": "/diem/index.html"
+  },
+  {
+    "revision": "020d4e07b5dc7b107e10",
+    "url": "/diem/static/css/main.06de1dd2.chunk.css"
+  },
+  {
+    "revision": "a2eb3877eea3ed9d079d",
+    "url": "/diem/static/js/2.94f4e4d5.chunk.js"
+  },
+  {
+    "revision": "020d4e07b5dc7b107e10",
+    "url": "/diem/static/js/main.3b68c614.chunk.js"
+  },
+  {
+    "revision": "7e160e66fb271df4a4f5",
+    "url": "/diem/static/js/runtime~main.7145cf2b.js"
+  },
+  {
+    "revision": "d60816825e8928df45052332de02a56e",
+    "url": "/diem/static/media/baseline-archive-24px.d6081682.svg"
+  },
+  {
+    "revision": "5c33ae76b89bb5c86418eac05d6d8389",
+    "url": "/diem/static/media/baseline-dehaze-24px.5c33ae76.svg"
+  },
+  {
+    "revision": "d36bd4a7a15f50d1a42ad58049254a48",
+    "url": "/diem/static/media/baseline-delete-24px.d36bd4a7.svg"
+  },
+  {
+    "revision": "8f39a211c8c294770581d5bfadf1836f",
+    "url": "/diem/static/media/baseline-expand_less-24px.8f39a211.svg"
+  },
+  {
+    "revision": "bb158ab64ed266b995914babb7ecf46a",
+    "url": "/diem/static/media/baseline-expand_more-24px.bb158ab6.svg"
+  },
+  {
+    "revision": "b9204e2e04e9214eef3321fdbcf0b9c7",
+    "url": "/diem/static/media/baseline-lens-24px.b9204e2e.svg"
+  },
+  {
+    "revision": "e27192cbdd7dbc4012199afa6ba27175",
+    "url": "/diem/static/media/baseline-location_on-24px.e27192cb.svg"
+  },
+  {
+    "revision": "4dc7a8cce1559386fa04e2b84289a9d4",
+    "url": "/diem/static/media/baseline-mail_outline-24px.4dc7a8cc.svg"
+  },
+  {
+    "revision": "3d195e25ba292b1a033a1aae71204132",
+    "url": "/diem/static/media/baseline-notifications_none-24px.3d195e25.svg"
+  },
+  {
+    "revision": "ff078facbceb66277bb8a61dc3da8f65",
+    "url": "/diem/static/media/baseline-phone-24px.ff078fac.svg"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/diem/static/media/baseline-search-24px.a87eca9e.svg"
+  },
+  {
+    "revision": "5be1f33130382cb7da32e61179f99059",
+    "url": "/diem/static/media/baseline-send-24px.5be1f331.svg"
+  },
+  {
+    "revision": "4044d87f6a8ff57ccad4616bee21ce5b",
+    "url": "/diem/static/media/map.4044d87f.png"
+  },
+  {
+    "revision": "096a7fd05e59739f9cd9e45e86fb7242",
+    "url": "/diem/static/media/nurse.096a7fd0.png"
+  },
+  {
+    "revision": "d0b509e7f43340a5c88d0d97537262e6",
+    "url": "/diem/static/media/outline-watch_later-24px.d0b509e7.svg"
+  }
+]);
